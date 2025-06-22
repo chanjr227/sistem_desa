bf822cd74cf7f5e72979b299b01653bd4aba96a0
chanjr227 <chandrajr227@gmail.com>
2025-06-22 21:43:15 +0700

    perbarui login

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['log']) || $_SESSION['role'] !== 'admin') {
+    header('Location: ../login.php');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
